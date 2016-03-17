1c2d1d71bef741b55e666173891c1a97da825a22
pbhadauria2000 <pbhadauria@hotmail.com>
2016-03-17 05:55:21 -0700

    Adding gulpfile.js and basic test

---
+++ gulpfile.js
@@ -0,0 +1,52 @@
+'use strict';
+
+var gulp = require('gulp');
+var mocha = require('gulp-mocha');
+var istanbul = require('gulp-istanbul');
+var runSequence = require('run-sequence');
+var eslint = require('gulp-eslint');
+var path     = require('path');
+var fs       = require('fs');
+
+
+
+gulp.task('eslint', function() {
+  return gulp.src(['index.js', 'lib/**/*.js'])
+  .pipe(eslint())
+  .pipe(eslint.format())
+  .pipe(eslint.failOnError());
+});
+
+// mocha test runner
+gulp.task('mochaTest', function (cb) {
+  gulp.src(['index.js','lib/**/*.js'])
+  .pipe(istanbul()) // Covering files
+  .pipe(istanbul.hookRequire()) // Force `require` to return covered files
+  .on('finish', function () {
+
+    process.chdir('./test/flexirestTest');
+    var app = require('./test/flexirestTest/server/server');
+
+    var serverInstance = app.start();
+    process.chdir('../../');
+    gulp.src(['test/index-test.js','test/lib/**/*-test.js'])
+    .pipe(mocha({ timeout: 10000 }))
+    .pipe(istanbul.writeReports({dir:'reports/coverage'}))// Creating the reports after tests runned
+    .on('end', function(){
+      serverInstance.close();
+      cb();
+    });
+  });
+});
+
+// default task
+gulp.task('default', function (callback) {
+  runSequence(
+    'mochaTest',
+    function (error) {
+      if (error) {
+        console.log(error.message);
+      }
+      callback(error);
+    });
+  });
